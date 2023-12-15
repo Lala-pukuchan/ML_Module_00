a91e593fa6b31e492de62267213fce2150d386b8
Ruru Kobayashi <rukobaya@made-f0Ar1s1.clusters.42paris.fr>
2023-12-15 11:34:58 +0100

    modify test

--- ex03/tools.py
+++ ex03/tools.py
@@ -18,8 +18,12 @@ def add_intercept(x):
 output_file = "results/ex03/result_ex03.txt"
 
 with open(output_file, "w") as file:
+    print("---test.1---", file=file)
     x = np.arange(1, 6)
-    print("Example 1:", add_intercept(x), file=file)
+    print("before add_intercept:\n", x, file=file)
+    print("afer add_intercept:\n", add_intercept(x), file=file)
 
+    print("\n---test.2---", file=file)
     y = np.arange(1, 10).reshape((3, 3))
-    print("Example 2:", add_intercept(y), file=file)
+    print("before add_intercept:\n", y, file=file)
+    print("afer add_intercept:\n", add_intercept(y), file=file)

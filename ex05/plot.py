dfae6204a8427c223a6fee16f66f3fac7e7bf722
Ruru Kobayashi <rukobaya@made-f0Ar1s1.clusters.42paris.fr>
2023-12-15 13:29:54 +0100

    ex05 test

--- ex05/plot.py
+++ ex05/plot.py
@@ -3,34 +3,45 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 # Use a non-interactive backend for matplotlib
-matplotlib.use("Agg")
+# matplotlib.use("Agg")
 
 
 def plot(x, y, theta, output_file):
-    """Plot the data and prediction line from three non-empty numpy.array.
+    """
+    Plot the data and prediction line from three non-empty numpy.array.
     Args:
-    x: has to be an numpy.array, a vector of dimension m * 1.
-    y: has to be an numpy.array, a vector of dimension m * 1.
-    theta: has to be an numpy.array, a vector of dimension 2 * 1.
+        x: has to be an numpy.array, a vector of dimension m * 1.
+        y: has to be an numpy.array, a vector of dimension m * 1.
+        theta: has to be an numpy.array, a vector of dimension 2 * 1.
     Returns:
         Nothing.
     Raises:
-    This function should not raise any Exceptions.
+        This function should not raise any Exceptions.
     """
+
+    # return if x, y, theta aren't array
     if not (
         isinstance(x, np.ndarray)
         and isinstance(y, np.ndarray)
         and isinstance(theta, np.ndarray)
     ):
         return
+    
+    # return if x, y, theta aren't array
     if x.size == 0 or y.size == 0 or theta.size == 0:
         return
-    if theta.shape != (2, 1) or x.ndim != 1:
-        return
-
-    # Reshape x to a 2D array if it's 1D
+    
+    # reshape if x, y, theta are array
     if x.ndim == 1:
         x = x.reshape(-1, 1)
+    if y.ndim == 1:
+        y = y.reshape(-1, 1)
+    if theta.ndim == 1:
+        theta = theta.reshape(-1, 1)
+
+    # check shape
+    if theta.shape != (2, 1) or x.shape[1] != 1 or y.shape[1] != 1 or x.shape[0] != y.shape[0]:
+        return
 
     # Compute the prediction
     y_hat = theta[0] + theta[1] * x
@@ -52,13 +63,6 @@ output_file_1 = "results/ex05/result_ex05_figure1"
 output_file_2 = "results/ex05/result_ex05_figure2"
 output_file_3 = "results/ex05/result_ex05_figure3"
 
-x = np.arange(1, 6)
-y = np.array([3.74013816, 3.61473236, 4.57655287, 4.66793434, 5.95585554])
-theta1 = np.array([[4.5], [-0.2]])
-plot(x, y, theta1, output_file_1)
-
-theta2 = np.array([[-1.5], [2]])
-plot(x, y, theta2, output_file_2)
-
-theta3 = np.array([[3], [0.3]])
-plot(x, y, theta3, output_file_3)
+plot(np.array([0, 1]), np.array([0, 1]), np.array([0, 1]), output_file_1)
+plot(np.array([0, 1]), np.array([0, 1]), np.array([1, 1]), output_file_2)
+plot(np.array([0, 2]), np.array([0, 0]), np.array([-1, 1]), output_file_3)
